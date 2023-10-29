b451a46081f30cbcf1fdfe26340712262f9312c1
Carl Sargunar <carl@sargunar.com>
2023-10-29 21:45:11 +0000

    product API

--- Src/UmbracoCMS/Controllers/ProductController.cs
+++ Src/UmbracoCMS/Controllers/ProductController.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Web.Common.Controllers;
 using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Extensions;
 using UmbracoCMS.Models;
 
 namespace UmbracoCMS.Controllers
@@ -17,11 +19,14 @@ namespace UmbracoCMS.Controllers
     {
         private readonly IUmbracoContextAccessor _umbracoContextAccessor;
         private readonly ILogger<ProductController> _logger;
+        private readonly IPublishedValueFallback _publishedValueFallback;
+        private string productDoctype = "product";
 
-        public ProductController(IUmbracoContextAccessor umbracoContextAccessor, ILogger<ProductController> logger)
+        public ProductController(IUmbracoContextAccessor umbracoContextAccessor, ILogger<ProductController> logger, IPublishedValueFallback publishedValueFallback)
         {
             _umbracoContextAccessor = umbracoContextAccessor;
             _logger = logger;
+            _publishedValueFallback = publishedValueFallback;
         }
 
         [HttpGet]
@@ -39,42 +44,25 @@ namespace UmbracoCMS.Controllers
                 _logger.LogCritical("Content Cache is null");
             }
             
-            var productType = context.Content.GetContentType("product");
-            var rootNode = context.Content.GetAtRoot();
-            Debug.WriteLine($"RootNode : {productType.Alias}");
+            var rootNode = context.Content.GetAtRoot().FirstOrDefault();
+            var productNodes = rootNode.DescendantsOfType(productDoctype);
+
+
             var tmp = new List<Product>();
-            
-            
-            
-            tmp.Add(new Product
-            {
-                Name = "Table",
-                Price = 100,
-                SKU = "TBL-001",
-                Image = "https://via.placeholder.com/150",
-                Description = "A table",
-                Category = new[] {"Table", "Furniture"}
-            });
 
-            tmp.Add(new Product
+            foreach (var p in productNodes)
             {
-                Name = "Chair",
-                Price = 50,
-                SKU = "CHR-001",
-                Image = "https://via.placeholder.com/150",
-                Description = "A chair",
-                Category = new[] {"Chair", "Furniture"}
-            });
+                if (p == null) continue;
+                var product = new Product
+                {
+                    Name = p.Name,
+                    Description = p.Value(_publishedValueFallback, "description") as string ?? string.Empty,
+                    Price = (decimal)(p.Value(_publishedValueFallback, "price") ?? 0),
+                    SKU = p.Value(_publishedValueFallback, "sku") as string ?? string.Empty,
+                };
+                tmp.Add(product);
+            }
 
-            tmp.Add(new Product
-            {
-                Name = "Desk",
-                Price = 200,
-                SKU = "DSK-001",
-                Image = "https://via.placeholder.com/150",
-                Description = "A desk",
-                Category = new[] {"Desk", "Furniture"}
-            });
 
             return tmp;
         }
