810a3d5afef94e4da9c4da96d8e42d9e3a28154d
Carl Sargunar <carl@sargunar.com>
2023-12-27 11:25:34 +0000

    remove old ver

--- Src/UmbracoCMS/Models/Content.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UmbracoCMS.Models
-{
-    public class Content
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- Src/UmbracoCMS/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UmbracoCMS.Models
-{
-    public class Person : Content
-    {
-        public string Image { get; set; }
-        public string[] Department { get; set; }
-
-    }
-}
