255ea46383d199d58f632702491706bad2cca693
Carl Sargunar <carl@sargunar.com>
2024-10-31 15:33:01 +0000

    Blazor Hybrid

---
+++ Src/MyBlazorHybridApp/Helpers/DemoHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMauiApp.Helpers
+{
+	internal static class DemoHelpers
+	{
+		internal static string BaseUrl = "http://localhost:4458/";
+
+		internal static string ContentDeliveryApiUrl = $"{BaseUrl}umbraco/delivery/api/v2/content?filter=contentType%3Aproduct";
+
+		internal static string ImagePath(string path)
+		{
+			if (string.IsNullOrEmpty(path))
+				return string.Empty;
+
+			return $"{BaseUrl}{path}?width=200";
+		}
+
+        // Service Bus
+        public const string ServiceBusConnectionString = "<TODO : REPLACE WITH YOUR BUS CONNECTIONSTRING>";
+        public const string ServiceBusQueueName = "<TODO : REPLACE WITH YOUR BUS QUEUE NAME>";
+    }
+}
---
+++ Src/MyBlazorHybridApp/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using MyMauiApp.Helpers;
+using MyMauiApp.Models;
+
+namespace MyMauiApp.Services
+{
+	public class ProductService
+	{
+
+		HttpClient httpClient;
+		public ProductService()
+		{
+			this.httpClient = new HttpClient();
+		}
+		
+
+		public async Task<List<Product>> GetProducts()
+		{
+			// Load Products from from the Content Delivery API
+			var productList = await FetchProductsFromContentDeliveryApi();
+			//var productList = await FetchLocalProducts();
+			return productList;
+		}
+
+        private async Task<List<Product>> FetchProductsFromContentDeliveryApi()
+        {
+            var products = new List<Product>();
+            var apiResponse = await httpClient.GetAsync(DemoHelpers.ContentDeliveryApiUrl);
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                var contentDeliveryResponse = await apiResponse.Content.ReadFromJsonAsync<ContentDeliveryResponse>();
+
+                foreach (var item in contentDeliveryResponse.items)
+                {
+                    var product = new Product
+                    {
+                        Name = item.name,
+                        Price = item.properties.price,
+                        Sku = item.properties.sku,
+                        Description = item.properties.description,
+                        Category = item.properties.category,
+                        Image = DemoHelpers.ImagePath(item.properties.photos[0].url)
+                    };
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+	}
+}
